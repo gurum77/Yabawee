375bde090ac697e1aa135059429b5aa1f8302f87
gurum77 <hileejaeho@gmail.com>
2021-07-23 06:23:29 +0900

    server browser ing..

--- scenes/Lobby.gd
+++ scenes/Lobby.gd
@@ -1,5 +1,6 @@
-extends Panel
+extends Control
 
+var player = load('res://scenes/Player.tscn')
 onready var main_ui = $MainUI
 onready var server_ip_address = $MainUI/ServerIPAddress
 onready var device_ip_address = $CanvasLayer/DeviceIPAddress
@@ -9,6 +10,7 @@ func _ready():
 	get_tree().connect('network_peer_connected', self, '_player_connected')
 	get_tree().connect('network_peer_disconnected', self, '_player_disconnected')
 	get_tree().connect('connected_to_server', self, '_connected_to_server')
+
 	device_ip_address.text = Network.ip_address
 
 # hosting을 시작함.
@@ -16,7 +18,11 @@ func _on_ButtonHost_pressed():
 	main_ui.hide()
 	Network.create_server()
 	
-
+	instance_player(get_tree().get_network_unique_id())
+	
+func _connected_to_server():
+	yield(get_tree().create_timer(0.1), 'timeout')
+	instance_player(get_tree().get_network_unique_id())
 # join
 func _on_ButtonJoin_pressed():
 	if server_ip_address.text != '':
@@ -26,7 +32,15 @@ func _on_ButtonJoin_pressed():
 	
 func _player_disconnected(id):
 	print('player ' + str(id) + ' has disconnected')
+	if Players.has_node(str(id)):
+		Players.get_node(str(id)).queue_free()
 	
 # player가 연결됨
 func _player_connected(id):
 	print('player ' + str(id) + ' has connected')
+	instance_player(id)
+	
+func instance_player(id):
+	var player_instance = Globals.instance_node_at_location(player, Players, Vector2(rand_range(0, 100), rand_range(0, 100)))
+	player_instance.name = str(id)
+	player_instance.set_network_master(id)
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,30 +1,33 @@
 extends KinematicBody2D
 
-var local_player = true
-var speed = 150
-var direction = Vector2()
+onready var tween = $Tween
+puppet var puppet_position = Vector2(0, 0) setget puppet_position_set
+puppet var puppet_velocity = Vector2()
+puppet var puppet_rotation = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-remote func _set_position(pos):
-	global_transform.origin = pos
-	
-func _physics_process(delta):
-	direction = Vector2()
-	
-	if(Input.is_action_pressed('move_left')):
-		direction += Vector2(-1, 0)
-	if(Input.is_action_pressed('move_right')):
-		direction += Vector2(1, 0)
-	if(Input.is_action_pressed('move_up')):
-		direction += Vector2(0, -1)
-	if(Input.is_action_pressed('move_down')):
-		direction += Vector2(0, 1)
-	
-	if direction != Vector2():
-		if local_player || is_network_master():
-			move_and_slide(direction * speed, Vector2.UP)
-		if !local_player:
-			rpc_unreliable('_set_position', global_transform.origin)
+var speed = 300
+var velocity = Vector2(0, 0)
+
+func _process(delta):
+	if is_network_master():
+		var x_input = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+		var y_input = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+		velocity = Vector2(x_input, y_input).normalized()
+		move_and_slide(velocity * speed)
+		look_at(get_global_mouse_position())
+	else:
+		rotation_degrees = lerp(rotation_degrees, puppet_rotation, delta*8)
+		if not tween.is_active():
+			move_and_slide(puppet_velocity * speed)
+
+func puppet_position_set(new_value)->void:
+	puppet_position = new_value
+	tween.interpolate_property(self, "global_position", global_position, puppet_position, 0.1)
+	tween.start()
+			
+func _on_Network_tick_rate_timeout():
+	if is_network_master():
+		rset_unreliable("puppet_position", global_position)
+		rset_unreliable("puppet_velocity", velocity)
+		rset_unreliable("puppet_rotation", rotation_degrees)
